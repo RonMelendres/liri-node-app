3a8e8b02e496313056f8419886aab7a2dabfa473
Ron Melendres <Ron.melendres25@gmail.com>
2019-09-26 20:46:58 -0700

    finished ombd portion

--- liri.js
+++ liri.js
@@ -8,39 +8,87 @@ var inquirer = require("inquirer");
 
 // var spotify = new Spotify(keys.spotify);
 
-var movieName = process.argv[2];
-
-var movieURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=ebe5df1e";
-
 // console.log(queryURL);
 
-axios.get(movieURL).then(
-    function (response) {
-        console.log("Movie title: " + response.data.Title);
-        console.log("Release Year: " + response.data.Year);
-        console.log("IMDB Rating: " + response.data.imdbRating);
-        console.log("Country of Production: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
-        // console.log(response);
-});
-    // .catch(function (error) {
-    //     // if (error.response) {
-    //     //     console.log("---------------Data---------------");
-    //     //     console.log(error.response.data);
-    //     //     console.log("---------------Status---------------");
-    //     //     console.log(error.response.status);
-    //     //     console.log("---------------Status---------------");
-    //     //     console.log(error.response.headers); }
-    //      if (error.request) {
-    //         // console.log(error.request);
-    //     } else {
+inquirer.prompt([
+    {
+        type: "input",
+        name: "userInput",
+        message: "Which movie would you like to search?"
+    } 
+]).then(function(response) {
+    console.log(response);
+    var movieURL = "http://www.omdbapi.com/?t=" + response.userInput + "&y=&plot=short&apikey=ebe5df1e";
+    
+
+    axios.get(movieURL).then(
+        function (response) {
+            console.log("Movie title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Country of Production: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
             
-    //         // console.log("Error", error.message);
-    //     }
-    //     // console.log(error.config);
-    // });
+        }).catch(function(error) {
+            if (error.response) {
+              
+              console.log("---------------Data---------------");
+              console.log(error.response.data);
+              console.log("---------------Status---------------");
+              console.log(error.response.status);
+              console.log("---------------Status---------------");
+              console.log(error.response.headers);
+            } else if (error.request) {
+             
+              console.log(error.request);
+            } else {
+              
+              console.log("Error", error.message);
+            }
+            console.log(error.config);
+          });
+        
+})
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "userInput",
+        message: "Which band would you like to search?"
+    } 
+]).then(function(response) {
+    console.log(response);
+    var bandURL = "https://rest.bandsintown.com/artists/" + response.userInput + "/events?app_id=codingbootcamp";
+
+
+    axios.get(bandURL).then(
+        function (response) {
+            console.log();
+            console.log();
+            console.log();
+    
+        }).catch(function(error) {
+            if (error.response) {
+              
+              console.log("---------------Data---------------");
+              console.log(error.response.data);
+              console.log("---------------Status---------------");
+              console.log(error.response.status);
+              console.log("---------------Status---------------");
+              console.log(error.response.headers);
+            } else if (error.request) {
+             
+              console.log(error.request);
+            } else {
+              
+              console.log("Error", error.message);
+            }
+            console.log(error.config);
+          });
+    
+})
 
 
 
@@ -53,58 +101,40 @@ axios.get(movieURL).then(
 
 
 
-// var bandURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-// axios.get(bandURL).then(
+
+
+
+// inquirer.prompt([
+//     {
+//         type: "input",
+//         name: "userInput",
+//         message: "Which song would you like to look up?"
+//     } 
+// ])
+
+// var spotifyURL = 
+
+// axios.get(spotifyURL).then(
 //     function (response) {
 //         console.log();
 //         console.log();
 //         console.log();
-        
+//         console.log();
 //     }
 // )
 
 
+// Artist(s)
 
 
+// The song's name
 
 
+// A preview link of the song from Spotify
 
 
-
-
-
-
-
-
-
-// var inputArr = process.argv;
-
-// // Parses the command line argument to capture the "operand" (add, subtract, multiply, etc) and the numbers
-// var operand = inputArr[2];
-// var num1 = inputArr[3];
-// var num2 = inputArr[4];
-
-
-// // var num1 = parseFloat(inputArr[3]);
-// // var num2 = parseFloat(inputArr[4]);
-
-
-// // Here's the variable we will be modifying with the new numbers
-// var outputNum;
-
-// // Determines the operand selected...
-// // Based on the operand we run the appropriate math on the two numbers
-// switch (operand) {
-// case "add":
-//   outputNum = parseFloat(num1) + parseFloat(num2);
-//   break;
-
-
-
-
-
-
+// The album that the song is from
 
 
 
