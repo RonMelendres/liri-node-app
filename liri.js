13c0c575bb34dc32a493633726114a354b1f6407
Ron Melendres <Ron.melendres25@gmail.com>
2019-09-26 22:43:08 -0700

    started spotify section

--- liri.js
+++ liri.js
@@ -8,51 +8,61 @@ var inquirer = require("inquirer");
 
 var moment = require('moment');
 
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
 
-// console.log(queryURL);
+var spotify = new Spotify(keys.spotify);
 
+inquirer.prompt([
+    {
+        type: "input",
+        name: "userInput",
+        message: "Which movie would you like to search?"
+    }
+
+]).then(function (response) {
+    console.log(response);
+    var movieURL = "http://www.omdbapi.com/?t=" + response.userInput + "&y=&plot=short&apikey=ebe5df1e";
+
+
+    axios.get(movieURL).then(
+        function (response) {
+            console.log("Movie title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Country of Production: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+
+        }).catch(function (error) {
+            if (error.response) {
+
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+
+                console.log(error.request);
+            } else {
+
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+
+})
 // inquirer.prompt([
-//     {
-//         type: "input",
-//         name: "userInput",
-//         message: "Which movie would you like to search?"
-//     } 
-// ]).then(function(response) {
-//     console.log(response);
-//     var movieURL = "http://www.omdbapi.com/?t=" + response.userInput + "&y=&plot=short&apikey=ebe5df1e";
-    
+//         {
+//             type: "confirm",
+//             message: "Would you like to continue?",
+//             name: "confirm",
+//             default: true
 
-//     axios.get(movieURL).then(
-//         function (response) {
-//             console.log("Movie title: " + response.data.Title);
-//             console.log("Release Year: " + response.data.Year);
-//             console.log("IMDB Rating: " + response.data.imdbRating);
-//             console.log("Country of Production: " + response.data.Country);
-//             console.log("Language: " + response.data.Language);
-//             console.log("Plot: " + response.data.Plot);
-//             console.log("Actors: " + response.data.Actors);
-            
-//         }).catch(function(error) {
-//             if (error.response) {
-              
-//               console.log("---------------Data---------------");
-//               console.log(error.response.data);
-//               console.log("---------------Status---------------");
-//               console.log(error.response.status);
-//               console.log("---------------Status---------------");
-//               console.log(error.response.headers);
-//             } else if (error.request) {
-             
-//               console.log(error.request);
-//             } else {
-              
-//               console.log("Error", error.message);
-//             }
-//             console.log(error.config);
-//           });
-        
-// })
+//         }
+//     ])
 
 inquirer.prompt([
     {
@@ -70,11 +80,11 @@ inquirer.prompt([
             console.log(response.data[0].venue);
             console.log("Venue: " + response.data[0].venue.name);
             console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"));
-            
-    
+
+
         }).catch(function(error) {
             if (error.response) {
-              
+
               console.log("---------------Data---------------");
               console.log(error.response.data);
               console.log("---------------Status---------------");
@@ -82,62 +92,56 @@ inquirer.prompt([
               console.log("---------------Status---------------");
               console.log(error.response.headers);
             } else if (error.request) {
-             
+
               console.log(error.request);
             } else {
-              
+
               console.log("Error", error.message);
             }
             console.log(error.config);
           });
-    
+
 })
 
+inquirer.prompt([
+    {
+        type: "input",
+        name: "userInput",
+        message: "Which song would you like to look up?"
+    }
+]).then(function (response) {
+    console.log(response);
+    var spotifyURL = "https://api.spotify.com/v1/search?query=" + response.userInput + "&offset=0&limit=10&type=";
 
+    axios.get(spotifyURL).then(
+        function (response) {
+            console.log(response);
+            console.log();
+            console.log();
+            console.log();
+            console.log();
 
+        }).catch(function (error) {
+            if (error.response) {
 
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
 
+                console.log(error.request);
+            } else {
 
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 
+})
 
-
-
-
-
-
-
-
-
-// inquirer.prompt([
-//     {
-//         type: "input",
-//         name: "userInput",
-//         message: "Which song would you like to look up?"
-//     } 
-// ])
-
-// var spotifyURL = 
-
-// axios.get(spotifyURL).then(
-//     function (response) {
-//         console.log();
-//         console.log();
-//         console.log();
-//         console.log();
-//     }
-// )
-
-
-// Artist(s)
-
-
-// The song's name
-
-
-// A preview link of the song from Spotify
-
-
-// The album that the song is from
 
 
 
